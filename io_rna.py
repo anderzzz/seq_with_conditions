207be257a8ce8421d0c9c6cb82146d10e6c75ef5
anderzzz <ohrn.anders@gmail.com>
2022-06-13 09:42:16 +0200

    Access to nucleotide record by custom output filter

--- io_rna.py
+++ io_rna.py
@@ -5,16 +5,29 @@ from pathlib import Path
 from torch.utils.data import Dataset
 from Bio import SeqIO
 
-class RNASequenceDataset(Dataset):
+class NucleotideSequenceDataset(Dataset):
 
-    def __init__(self, source_directory, file_pattern='*.gb'):
-        super(RNASequenceDataset, self).__init__()
+    def __init__(self, source_directory,
+                 input_file_pattern='*.gb',
+                 input_seqio_format='genbank',
+                 output_record_filter_func='seq_with_id'):
+        super(NucleotideSequenceDataset, self).__init__()
 
         p = Path(source_directory)
         if not p.is_dir():
             raise ValueError('File folder {} not found'.format(source_directory))
 
-        self.file_paths = list(p.glob(file_pattern))
+        self.file_paths = list(p.glob(input_file_pattern))
+        self.input_seqio_format = input_seqio_format
+        if output_record_filter_func == 'seq_with_id':
+            self.filter_record_ = self._filter_record_seq_with_id
+        elif output_record_filter_func == 'full_record':
+            self.filter_record_ = lambda x: x
+        elif callable(output_record_filter_func):
+            self.filter_record_ = output_record_filter_func
+        else:
+            raise ValueError('Unknown value for record filter parameter: {}'.format(output_record_filter_func))
+
         self.seq_mapper = {}
         index = 0
         for ff in self.file_paths:
@@ -30,12 +43,15 @@ class RNASequenceDataset(Dataset):
 
     def __getitem__(self, item):
         fp, record_id = self.seq_mapper[item]
-        records = self._read_file_(fp)
+        records = self._read_file_(fp, format_name=self.input_seqio_format)
 
         counter = int(record_id.split('.')[-1])
         record = records[counter - 1]
 
-        return record.seq.__str__(), record.id, record.name
+        return self.filter_record_(record)
 
     def _read_file_(self, fp, format_name='genbank'):
         return [record for record in SeqIO.parse(fp, format_name)]
+
+    def _filter_record_seq_with_id(self, record):
+        return record.seq.__str__(), record.id, record.name
--- tests/test_rnadataset.py
+++ tests/test_rnadataset.py
@@ -1,12 +1,14 @@
 '''Bla bla
 
 '''
-from io_rna import RNASequenceDataset
+from io_rna import NucleotideSequenceDataset
+from torch.utils.data import DataLoader
 
 TEST_DATA = '../test_data/'
 def test_simple_read():
-    data = RNASequenceDataset(TEST_DATA)
-    for dd in data:
+    data = NucleotideSequenceDataset(TEST_DATA)
+    dloader = DataLoader(data)
+    for dd in dloader:
         print (dd)
 
 if __name__ == '__main__':
